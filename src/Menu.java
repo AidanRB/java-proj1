d0880d345dc8a92f58ab7e37104f2360675240f0
AidanRB <aidanrichbennett@gmail.com>
2022-02-28 09:47:21 -0500

    Sanitize input

--- src/Menu.java
+++ src/Menu.java
@@ -7,9 +7,8 @@ public class Menu {
         while (true) {
             System.out.println("\n0. Quit");
             System.out.println("1. Create Employee");
-            System.out.print(": ");
             
-            int choice = scanner.nextInt();
+            int choice = getInt(0, 1);
             if (choice == 0) {
                 break;
             } else if (choice == 1) {
@@ -29,27 +28,26 @@ public class Menu {
             System.out.println("3. Set age");
             System.out.println("4. Preview Payday Statement");
             System.out.println("5. Retirement Help");
-            System.out.print(": ");
     
-            int choice = scanner.nextInt();
+            int choice = getInt(0, 5);
             
             switch (choice) {
                 case 0:
                     running = false;
                     break;
                 case 1:
-                    System.out.print("Monthly salary: ");
-                    double monthlySalary = scanner.nextDouble();
+                    System.out.print("Monthly salary");
+                    double monthlySalary = getDouble(0);
                     newEmployee.setMonthlySalary(monthlySalary);
                     break;
                 case 2:
-                    System.out.print("Annual salary: ");
-                    double yearlySalary = scanner.nextDouble();
+                    System.out.print("Annual salary");
+                    double yearlySalary = getDouble(0);
                     newEmployee.setYearlySalary(yearlySalary);
                     break;
                 case 3:
-                    System.out.println("Age: ");
-                    int age = scanner.nextInt();
+                    System.out.println("Age");
+                    int age = getInt(0, 120);
                     newEmployee.setAge(age);
                     break;
                 case 4:
@@ -74,16 +72,18 @@ public class Menu {
             System.out.println("1. Set Retirement Percentage");
             System.out.println("2. View Retirement Scenarios");
 
-            int choice = scanner.nextInt();
+            int choice = getInt(0, 2);
 
             switch (choice) {
                 case 0:
                     running = false;
                     break;
                 case 1:
-                    System.out.print("Retirement percentage: ");
-                    int retirementPercent = scanner.nextInt();
+                    System.out.print("Retirement percentage");
+                    int retirementPercent = getInt(0, 100);
                     employee.setRetirementPercent(retirementPercent);
+                    break;
+                case 2:
                     break;
             
                 default:
@@ -93,4 +93,22 @@ public class Menu {
         
         return employee;
     }
+
+    public static int getInt(int low, int high) {
+        int input = low - 1;
+        while (input < low || input > high) {
+            System.out.print(": ");
+            input = scanner.nextInt();
+        }
+        return input;
+    }
+
+    public static double getDouble(double low) {
+        double input = low - 1;
+        while (input <= low) {
+            System.out.print(": ");
+            input = scanner.nextDouble();
+        }
+        return input;
+    }    
 }
